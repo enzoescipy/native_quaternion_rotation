908d67f692cbffdecb020994ab406c14a50ddaa1
enzoescipy <enzoescipy@gmail.com>
2024-04-11 13:20:27 +0900

    temporary commit for 2024-04-11

--- android/src/main/kotlin/com/example/native_quaternion_rotation/NativeQuaternionRotationPlugin.kt
+++ android/src/main/kotlin/com/example/native_quaternion_rotation/NativeQuaternionRotationPlugin.kt
@@ -1,6 +1,7 @@
 package com.example.native_quaternion_rotation
 
 import android.app.Activity
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -15,24 +16,26 @@ import io.reactivex.rxjava3.disposables.Disposable
 
 
 /** NativeQuaternionRotationPlugin */
-class NativeQuaternionRotationPlugin: FlutterPlugin, MethodCallHandler {
+class NativeQuaternionRotationPlugin: FlutterPlugin, SensorEventListener {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
   private lateinit var eventChannel : EventChannel
   private var disposable: Disposable? = null
 
-  private lateinit var mSensorManager : SensorManager
+  private lateinit var  mSensorManager: SensorManager
+  private var mQuaternion: Sensor? = null
+
   private val NATIVE_QUATERNION_ROTATION_EVENVT_CHANNEL =  "native_quaternion_rotation_event_channel"
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_quaternion_rotation")
-    channel.setMethodCallHandler(this)
+    mSensorManager = flutterPluginBinding.applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
-//    mSensorManager =  flutterPluginBinding.applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-//    eventChannel = EventChannel(flutterPluginBinding.)
+    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, NATIVE_QUATERNION_ROTATION_EVENVT_CHANNEL)
+
+    mSensorManager.registerListener(this, mQuaternion, SensorManager.SENSOR_DELAY_NORMAL)
   }
 
   private fun observeQuaternion(events: EventChannel.EventSink?) {
@@ -46,41 +49,33 @@ class NativeQuaternionRotationPlugin: FlutterPlugin, MethodCallHandler {
     return Sensor.TYPE_ROTATION
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
-  }
-
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    mSensorManager.unregisterListener(this)
   }
 }
 
 
-//class SensorActivity : Activity(), SensorEventListener {
-//  private val mSensorManager: SensorManager
-//  private val mQuaternion: Sensor?
-//
-//  init {
-//    mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-//    mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
-//  }
-//
-//  override fun onResume() {
-//    super.onResume()
-//    mSensorManager.registerListener(this, mQuaternion, SensorManager.SENSOR_DELAY_NORMAL)
-//  }
-//
-//  override fun onPause() {
-//    super.onPause()
-//    mSensorManager.unregisterListener(this)
-//  }
-//
-//  override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
-//  override fun onSensorChanged(event: SensorEvent) {}
-//}
+class SensorActivity : Activity(), SensorEventListener {
+  private val mSensorManager: SensorManager
+  private val mQuaternion: Sensor?
+
+  init {
+    mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+    mQuaternion = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    mSensorManager.registerListener(this, mQuaternion, SensorManager.SENSOR_DELAY_NORMAL)
+  }
+
+  override fun onPause() {
+    super.onPause()
+    mSensorManager.unregisterListener(this)
+  }
+
+  override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+  override fun onSensorChanged(event: SensorEvent) {}
+}
 
 
